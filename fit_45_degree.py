2cac2594fa8491a19024bd04a142b2f5da831974
Lee Hagaman <leehagaman@Lees-MacBook-Pro.local>
2017-05-31 20:32:32 -0700

    changed data structure to work with points of independent parameters rather than theta_r arrays each with common other independent variables. fitting multiple tests should work much better now

--- fit_45_degree.py
+++ fit_45_degree.py
@@ -1,21 +1,33 @@
-from fitting_library import plot_data
+from fitting_library import plot_data_with_fit
 import numpy as np
 
 data = np.loadtxt("opticalExperiment/45_degree_IBA_in_air.txt", skiprows=1)
 
-xdata = [d[0] for d in data]
-ydata = [d[1] for d in data]
+theta_r_array = [d[0] for d in data]
+intensity_array = [d[1] for d in data]
 
+"""
 # if the lines from back to front annoy you:
-xdata_partial = xdata[0:len(xdata) / 5]
-ydata_partial = ydata[0:len(ydata) / 5]
+theta_r_array = theta_r_array[0:len(theta_r_array) / 5]
+intensity_array = intensity_array[0:len(intensity_array) / 5]
+"""
+
+numpoints = len(theta_r_array)
 
 # air
 n_0 = 1
+
 # manually chosen, would normally be known during experiment,
 # but I changed it because error on this angle was large
 theta_i = 54
+
 # phi_r=0 for all measurements we'll make
 phi_r = 0
 
-plot_data(xdata, ydata, n_0, theta_i, phi_r, "45 degrees IBA in air")
+points = []
+for i in range(numpoints):
+    theta_r = theta_r_array[i]
+    intensity = intensity_array[i]
+    points.append([theta_r, phi_r, theta_i, n_0, intensity])
+
+plot_data_with_fit(points, "45 degrees IBA in air")
--- fitting_library.py
+++ fitting_library.py
@@ -14,35 +14,22 @@ def H(x):
     return 0.5 * (np.sign(x) + 1)
 
 
-class MultiFitter:
-    # takes data = [[xdata1, ydata1, n_0_1, theta_i1, phi_r1], [...], ...]
-    def __init__(self, data):
-        self.data = data
-
-    # BRIDF with n definitely greater than one and rho_L positive to be used for fitting
-    # piecewise BRIDF of each test sequentially
-    def fitter(self, theta_r_in_degrees_array, log_rho_L, log_n_minus_one, log_gamma):
-        arr = []
-        for data in self.data:
-            arr += BRIDF(theta_r_in_degrees_array, data[4], data[3], data[2],
-                        np.exp(log_rho_L), np.exp(log_n_minus_one) + 1, np.exp(log_gamma))
-        return [x[0] + x[1] for x in arr]
-
-
 # BRIDF as described in paper
+# takes an array, easy to plot
 # returns [[specular1, diffuse1], [specular2, diffuse2], ...]
-def BRIDF(theta_r_in_degrees_array, phi_r, theta_i_in_degrees, n_0, rho_L, n, gamma):
+def BRIDF_plotter(theta_r_in_degrees_array, phi_r_in_degrees, theta_i_in_degrees, n_0, rho_L, n, gamma):
+    phi_r = phi_r_in_degrees * np.pi / 180
     theta_i = theta_i_in_degrees * np.pi / 180
     return_array = []
     for theta_r_in_degrees in theta_r_in_degrees_array:
-        arr = BRIDF_helper(np.pi * theta_r_in_degrees / 180, phi_r, theta_i, n_0, rho_L, n, gamma)
+        arr = BRIDF(np.pi * theta_r_in_degrees / 180, phi_r, theta_i, n_0, rho_L, n, gamma)
         return_array.append([arr[0], arr[1]])
     return return_array
 
 
-# uses radians, has a random alpha already chosen
+# uses radians, takes a single point
 # returns [specular, diffuse]
-def BRIDF_helper(theta_r, phi_r, theta_i, n_0, rho_L, n, gamma):
+def BRIDF(theta_r, phi_r, theta_i, n_0, rho_L, n, gamma):
     theta_i_prime = 0.5 * np.arccos(np.cos(theta_i) * np.cos(theta_r) -
         np.sin(theta_i) * np.sin(theta_r) * np.cos(phi_r))
 
@@ -84,52 +71,71 @@ def BRIDF_helper(theta_r, phi_r, theta_i, n_0, rho_L, n, gamma):
         rho_L / np.pi * W * (1 - A + B) * np.cos(theta_r)]
 
 
-# used to fit parameters for multiple incident angles and n_0
-# takes data = [[xdata1, ydata1, n_0_1, theta_i1, phi_r1], [...], ...]
-# returns [rho_L, n, gamma]
-# haven't tested this with multiple datasets yet, waiting for more data
-def multi_fit_parameters(data):
-    fit = MultiFitter(data)
+# independent variables has the form [theta_r_in_degrees, phi_r_in_degrees, theta_i_in_degrees, n_0]
+def unvectorized_fitter(independent_variables, log_rho_L, log_n_minus_one, log_gamma):
+    theta_r = independent_variables[0] * np.pi / 180
+    phi_r = independent_variables[1] * np.pi / 180
+    theta_i = independent_variables[2] * np.pi / 180
+    n_0 = independent_variables[3]
+    rho_L = np.exp(log_rho_L)
+    n = np.exp(log_n_minus_one) + 1
+    gamma = np.exp(log_gamma)
+    pair = BRIDF(theta_r, phi_r, theta_i, n_0, rho_L, n, gamma)
+    return pair[0] + pair[1]
 
-    totalxdata = []
-    totalydata = []
 
-    for x in data:
-        totalxdata += x[0]
-        totalydata += x[1]
+# takes arrays of independent variable lists
+def fitter(independent_variables_array, log_rho_L, log_n_minus_one, log_gamma):
+    arr = []
+    for independent_variables in independent_variables_array:
+        arr.append(unvectorized_fitter(independent_variables, log_rho_L, log_n_minus_one, log_gamma))
+    return arr
 
-    fit_params = scipy.optimize.curve_fit(fit.fitter, totalxdata, totalydata,
+
+# each point has form [theta_r_in_degrees, phi_r_in_degrees, theta_i_in_degrees, n_0, intensity]
+def fit_parameters(points):
+    independent_variables_array = []
+    intensity_array = []
+    for point in points:
+        independent_variables_array.append(point[:4])
+        intensity_array.append(point[4])
+    # initial parameters are the ones found in the paper
+    fit_params = scipy.optimize.curve_fit(fitter, independent_variables_array, intensity_array,
                                           p0=[np.log(0.5), np.log(1.5 - 1), np.log(0.05)])[0]
-    return [np.exp(fit_params[0]), np.exp(fit_params[1]) + 1, np.exp(fit_params[2])]
+    return [np.exp(fit_params[0]), np.exp(fit_params)[1] + 1, np.exp(fit_params[2])]
 
 
-# for plotting just one theta_i and n_0
-def plot_data(xdata, ydata, n_0, theta_i, phi_r, title):
+# works for plotting curves with constant phi_r, theta_i, and n_0
+def plot_data_with_fit(points, title):
 
-    data = [[xdata, ydata, n_0, theta_i, phi_r]]
+    phi_r_in_degrees = points[0][1]
+    theta_i_in_degrees = points[0][2]
+    n_0 = points[0][3]
 
-    totalxdata = []
-    totalydata = []
+    parameters = fit_parameters(points)
 
-    for x in data:
-        totalxdata += x[0]
-        totalydata += x[1]
+    rho_L = parameters[0]
+    n = parameters[1]
+    gamma = parameters[2]
 
-    fit_params = multi_fit_parameters(data)
+    theta_r_in_degrees_array = []
+    intensity_array = []
+    for point in points:
+        theta_r_in_degrees_array.append(point[0])
+        intensity_array.append(point[4])
 
-    fitted_data = BRIDF(xdata, phi_r, theta_i, n_0, fit_params[0], fit_params[1], fit_params[2])
+    fitted_data = BRIDF_plotter(theta_r_in_degrees_array, phi_r_in_degrees, theta_i_in_degrees, n_0, rho_L, n, gamma)
 
-    plt.plot(totalxdata, totalydata, label="experimental")
-    plt.plot(totalxdata, [x[0] for x in fitted_data], label="fitted specular")
-    plt.plot(totalxdata, [x[1] for x in fitted_data], label="fitted diffuse")
-    plt.plot(totalxdata, [x[0] + x[1] for x in fitted_data], label="fitted total")
+    plt.plot(theta_r_in_degrees_array, intensity_array, label="experimental")
+    plt.plot(theta_r_in_degrees_array, [x[0] for x in fitted_data], label="fitted specular")
+    plt.plot(theta_r_in_degrees_array, [x[1] for x in fitted_data], label="fitted diffuse")
+    plt.plot(theta_r_in_degrees_array, [x[0] + x[1] for x in fitted_data], label="fitted total")
 
     plt.title(title)
     plt.legend(bbox_to_anchor=(0.9, 0.9), bbox_transform=plt.gcf().transFigure)
     plt.ylabel("relative intensity")
     plt.xlabel("viewing angle (degrees)")
-    string = "theta_i: " + str(theta_i) + "\nrho_L: " + str(fit_params[0]) + "\nn: " + \
-        str(fit_params[1]) + "\ngamma: " + str(fit_params[2])
+    string = "theta_i: " + str(theta_i_in_degrees) + "\nrho_L: " + str(rho_L) + "\nn: " + \
+             str(n) + "\ngamma: " + str(gamma)
     plt.annotate(string, xy=(0.05, 0.8), xycoords='axes fraction')
     plt.show()
-
