b31114a9ebafcb64c2134a92a6abbbe7e92b53cd
Lee Hagaman <leehagaman@Lees-MacBook-Pro.local>
2017-06-01 18:31:27 -0700

    added gaussian model, made several plotting options

--- fit_45_degree.py
+++ fit_45_degree.py
@@ -1,4 +1,5 @@
 import torrance_sparrow_fit
+import gaussian_fit
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -25,17 +26,17 @@ theta_i_in_degrees = 54
 # phi_r=0 for all measurements we'll make
 phi_r_in_degrees = 0
 
-polarization = 0
 
-for polarization in [0, 1, 2]:
-    plt.figure(polarization)
-    if (polarization == 0):
-        p = "unpolarized"
-    elif (polarization == 1):
-        p = "s-polarized"
-    elif (polarization == 2):
-        p = "p-polarized"
-    title = "45 degree IBA in air, Torrance-Sparrow Fit, " + p
+plot_torrance_sparrow_unpolarized = False
+plot_torrance_sparrow_polarizations = False
+plot_gaussian = False
+plot_torrance_sparrow_unpolarized_and_gaussian = True
+
+if plot_torrance_sparrow_unpolarized:
+
+    polarization = 0
+
+    title = "45 degree IBA in air, Torrance-Sparrow Fit, unpolarized"
 
     # each point has form [theta_r_in_degrees, phi_r_in_degrees, theta_i_in_degrees, n_0, polarization, intensity]
     points = []
@@ -71,5 +72,149 @@ for polarization in [0, 1, 2]:
     string = "theta_i: " + str(theta_i_in_degrees) + "\nrho_L: " + str(rho_L) + "\nn: " + \
              str(n) + "\ngamma: " + str(gamma)
     plt.annotate(string, xy=(0.05, 0.8), xycoords='axes fraction')
+
+    plt.show()
+
+if plot_torrance_sparrow_polarizations:
+
+    for polarization in [0, 1, 2]:
+
+        plt.figure(polarization)
+
+        if (polarization == 0):
+            p = "unpolarized"
+        elif (polarization == 1):
+            p = "s-polarized"
+        elif (polarization == 2):
+            p = "p-polarized"
+        title = "45 degree IBA in air, Torrance-Sparrow Fit, " + p
+
+        # each point has form [theta_r_in_degrees, phi_r_in_degrees, theta_i_in_degrees, n_0, polarization, intensity]
+        points = []
+        for i in range(numpoints):
+            theta_r = theta_r_array[i]
+            intensity = intensity_array[i]
+            points.append([theta_r, phi_r_in_degrees, theta_i_in_degrees, n_0, polarization, intensity])
+
+        theta_r_in_degrees_array = []
+        intensity_array = []
+        for point in points:
+            theta_r_in_degrees_array.append(point[0])
+            intensity_array.append(point[5])
+
+        parameters = torrance_sparrow_fit.fit_parameters(points)
+
+        rho_L = parameters[0]
+        n = parameters[1]
+        gamma = parameters[2]
+
+        fitted_data = torrance_sparrow_fit.BRIDF_plotter(
+            theta_r_in_degrees_array, phi_r_in_degrees, theta_i_in_degrees, n_0, rho_L, n, gamma, polarization)
+
+        plt.plot(theta_r_in_degrees_array, intensity_array, label="experimental")
+        # plt.plot(theta_r_in_degrees_array, [x[0] for x in fitted_data], label="fitted specular")
+        # plt.plot(theta_r_in_degrees_array, [x[1] for x in fitted_data], label="fitted diffuse")
+        plt.plot(theta_r_in_degrees_array, [x[0] + x[1] for x in fitted_data], label="fitted intensity")
+
+        plt.title(title)
+        plt.legend(bbox_to_anchor=(0.9, 0.9), bbox_transform=plt.gcf().transFigure)
+        plt.ylabel("relative intensity")
+        plt.xlabel("viewing angle (degrees)")
+        string = "theta_i: " + str(theta_i_in_degrees) + "\nrho_L: " + str(rho_L) + "\nn: " + \
+                 str(n) + "\ngamma: " + str(gamma)
+        plt.annotate(string, xy=(0.05, 0.8), xycoords='axes fraction')
+
+if plot_gaussian:
+
+    plt.figure()
+
+    # always arbitrary for gaussian fit
+    # (actual polarization will likely affect R1 and R2 though)
+    polarization = 0
+
+    title = "45 degree IBA in air, Gaussian Fit"
+
+    # each point has form [theta_r_in_degrees, phi_r_in_degrees, theta_i_in_degrees, n_0, polarization, intensity]
+    points = []
+    for i in range(numpoints):
+        theta_r = theta_r_array[i]
+        intensity = intensity_array[i]
+        points.append([theta_r, phi_r_in_degrees, theta_i_in_degrees, n_0, polarization, intensity])
+
+    theta_r_in_degrees_array = []
+    intensity_array = []
+    for point in points:
+        theta_r_in_degrees_array.append(point[0])
+        intensity_array.append(point[5])
+
+    parameters = gaussian_fit.fit_parameters(points)
+
+    sigma = parameters[0]
+    R_1 = parameters[1]
+    R_2 = parameters[2]
+
+    fitted_data = gaussian_fit.BRIDF_plotter(theta_r_in_degrees_array, theta_i_in_degrees, sigma, R_1, R_2)
+
+    plt.plot(theta_r_in_degrees_array, intensity_array, label="experimental")
+    #plt.plot(theta_r_in_degrees_array, [x[0] for x in fitted_data], label="fitted specular")
+    #plt.plot(theta_r_in_degrees_array, [x[1] for x in fitted_data], label="fitted diffuse")
+    plt.plot(theta_r_in_degrees_array, [x[0] + x[1] for x in fitted_data], label="fitted intensity")
+
+    plt.title(title)
+    plt.legend(bbox_to_anchor=(0.9, 0.9), bbox_transform=plt.gcf().transFigure)
+    plt.ylabel("relative intensity")
+    plt.xlabel("viewing angle (degrees)")
+    string = "theta_i: " + str(theta_i_in_degrees) + "\nsigma: " + str(sigma) + "\nR_1: " + \
+             str(R_1) + "\nR_2: " + str(R_2)
+    plt.annotate(string, xy=(0.05, 0.8), xycoords='axes fraction')
+
+
+if plot_torrance_sparrow_unpolarized_and_gaussian:
+
+    title = "45 degree IBA in air, Torrance-Sparrow unpolarized and Gaussian Fit"
+
+    polarization = 0
+
+    # each point has form [theta_r_in_degrees, phi_r_in_degrees, theta_i_in_degrees, n_0, polarization, intensity]
+    points = []
+    for i in range(numpoints):
+        theta_r = theta_r_array[i]
+        intensity = intensity_array[i]
+        points.append([theta_r, phi_r_in_degrees, theta_i_in_degrees, n_0, polarization, intensity])
+
+    theta_r_in_degrees_array = []
+    intensity_array = []
+    for point in points:
+        theta_r_in_degrees_array.append(point[0])
+        intensity_array.append(point[5])
+
+    gaussian_parameters = gaussian_fit.fit_parameters(points)
+    sigma = gaussian_parameters[0]
+    R_1 = gaussian_parameters[1]
+    R_2 = gaussian_parameters[2]
+
+    torrance_sparrow_parameters = torrance_sparrow_fit.fit_parameters(points)
+    rho_L = torrance_sparrow_parameters[0]
+    n = torrance_sparrow_parameters[1]
+    gamma = torrance_sparrow_parameters[2]
+
+    gaussian_fitted_data = gaussian_fit.BRIDF_plotter(theta_r_in_degrees_array, theta_i_in_degrees, sigma, R_1, R_2)
+
+    torrance_sparrow_fitted_data = torrance_sparrow_fit.BRIDF_plotter(
+        theta_r_in_degrees_array, phi_r_in_degrees, theta_i_in_degrees, n_0, rho_L, n, gamma, polarization)
+
+    plt.plot(theta_r_in_degrees_array, intensity_array, label="experimental")
+    plt.plot(theta_r_in_degrees_array, [x[0] + x[1] for x in gaussian_fitted_data], label="Gaussian model")
+    plt.plot(theta_r_in_degrees_array, [x[0] + x[1] for x in torrance_sparrow_fitted_data],
+             label="Torrance-Sparrow model")
+
+    plt.title(title)
+    plt.legend(bbox_to_anchor=(0.9, 0.9), bbox_transform=plt.gcf().transFigure)
+    plt.ylabel("relative intensity")
+    plt.xlabel("viewing angle (degrees)")
+    string = "theta_i: " + str(theta_i_in_degrees) + "\n\nsigma: " + str(sigma) + "\nR_1: " + \
+             str(R_1) + "\nR_2: " + str(R_2) + "\n\nrho_L: " + str(rho_L) + "\nn: " + \
+                 str(n) + "\ngamma: " + str(gamma)
+    plt.annotate(string, xy=(0.05, 0.6), xycoords='axes fraction')
 
 plt.show()
---
+++ gaussian_fit.py
@@ -0,0 +1,57 @@
+# using fit from chapter 6 of Levy thesis
+# assumes theta_tilt (phi_r with other convention) is zero
+
+#TODO: make it only work for fitting one measure of theta_rot at a time
+
+import numpy as np
+import scipy.optimize
+
+
+# uses radians
+# returns [specular, diffuse]
+# only works for phi_r = 0
+# R_1 and R_2 are functions of theta_rot
+def BRIDF(theta_PMT, theta_rot, sigma, R_1, R_2):
+    alpha = np.arccos(np.sin(theta_rot) * np.sin(theta_PMT) + np.cos(theta_rot) * np.cos(theta_PMT))
+    return [R_1 * np.exp(-np.power(alpha, 2) / (2 * np.power(sigma, 2))), R_2 * np.cos(theta_PMT)]
+
+
+# takes an array, easy to plot
+# returns [[specular1, diffuse1], [specular2, diffuse2], ...]
+def BRIDF_plotter(theta_PMT_in_degrees_array, theta_rot_in_degrees, sigma, R_1, R_2):
+    theta_rot = theta_rot_in_degrees * np.pi / 180
+    return_array = []
+    for theta_rot_in_degrees in theta_PMT_in_degrees_array:
+        theta_PMT = np.pi * theta_rot_in_degrees / 180
+        arr = BRIDF(theta_rot, theta_PMT, sigma, R_1, R_2)
+        return_array.append([arr[0], arr[1]])
+    return return_array
+
+
+def unvectorized_fitter(independent_variables, sigma, R_1, R_2):
+    theta_PMT = independent_variables[0] * np.pi / 180
+    theta_rot = independent_variables[1] * np.pi / 180
+    pair = BRIDF(theta_PMT, theta_rot, sigma, R_1, R_2)
+    return pair[0] + pair[1]
+
+
+# each independent_variables has the form [theta_PMT_in_degrees, theta_rot_in_degrees]
+def fitter(independent_variables_array, sigma, R_1, R_2):
+    arr = []
+    for independent_variables in independent_variables_array:
+        arr.append(unvectorized_fitter(independent_variables, sigma, R_1, R_2))
+    return arr
+
+
+# each point has form [theta_PMT_in_degrees, phi_r_in_degrees, theta_rot_in_degrees, n_0, polarization, intensity]
+# we only care about theta_PMT_in_degrees, theta_rot_in_degrees, intensity
+def fit_parameters(points):
+    independent_variables_array = []
+    intensity_array = []
+    for point in points:
+        independent_variables_array.append([point[0], point[2]])
+        intensity_array.append(point[5])
+    # initial parameters are approximately the ones found in the paper
+    fit_params = scipy.optimize.curve_fit(fitter, independent_variables_array, intensity_array,
+                                          p0=[0.1, 100, 50])[0]
+    return [fit_params[0], fit_params[1], fit_params[2]]
