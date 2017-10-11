fixed TSTR model using average incident angle rather than specific incident angle for the microfacet for each microfacet. Also experimented with averaging over a range of angles.

TSTR_fit.py
@@ -85,6 +85,45 @@ def BRIDF_plotter(theta_r_in_degrees_array, phi_r_in_degrees, theta_i_in_degrees
     return return_array
 
 
+
+def BRIDF_plotter(theta_r_in_degrees_array, phi_r_in_degrees, theta_i_in_degrees, n_0, polarization, parameters, average_angle=0):
+    phi_r = phi_r_in_degrees * np.pi / 180
+    theta_i = theta_i_in_degrees * np.pi / 180
+    return_array = []
+    for theta_r_in_degrees in theta_r_in_degrees_array:
+        theta_r = np.pi * theta_r_in_degrees / 180
+        return_array.append(BRIDF(theta_r, phi_r, theta_i, n_0, polarization, parameters))
+    if average_angle == 0:
+        return return_array
+    else:
+        grouped = [[] for i in return_array]
+        for i in range(len(return_array)):
+            for j in range(len(return_array)):
+                if np.abs(theta_r_in_degrees_array[j] - theta_r_in_degrees_array[i]) < average_angle / 2.:
+                    grouped[i].append(return_array[j])
+        averaged = [np.average(grouped[i]) for i in range(len(return_array))]
+        return averaged
+
+
+def average_by_angle(theta_r_in_degrees_array, intensity_array, angle):
+    grouped = [[] for i in intensity_array]
+    for i in range(len(intensity_array)):
+        for j in range(len(intensity_array)):
+            if np.abs(theta_r_in_degrees_array[j] - theta_r_in_degrees_array[i]) < angle / 2.:
+                grouped[i].append(intensity_array[j])
+    averaged = [np.average(grouped[i]) for i in range(len(intensity_array))]
+    return averaged
+
+def BRIDF_radiance_plotter(theta_r_in_degrees_array, phi_r_in_degrees, theta_i_in_degrees, n_0, polarization, parameters):
+    phi_r = phi_r_in_degrees * np.pi / 180
+    theta_i = theta_i_in_degrees * np.pi / 180
+    return_array = []
+    for theta_r_in_degrees in theta_r_in_degrees_array:
+        theta_r = np.pi * theta_r_in_degrees / 180
+        return_array.append(BRIDF(theta_r, phi_r, theta_i, n_0, polarization, parameters) / np.cos(theta_r))
+    return return_array
+
+
 def BRIDF_specular_plotter(theta_r_in_degrees_array, phi_r_in_degrees, theta_i_in_degrees,
                            n_0, polarization, parameters):
     phi_r = phi_r_in_degrees * np.pi / 180
@@ -108,15 +147,17 @@ def BRIDF_diffuse_plotter(theta_r_in_degrees_array, phi_r_in_degrees, theta_i_in
 
 
 def BRIDF_pair(theta_r, phi_r, theta_i, n_0, polarization, parameters):
+
     rho_L = parameters[0]
     n = parameters[1]
     gamma = parameters[2]
 
-    theta_i_prime = 0.5 * np.arccos(np.cos(theta_i) * np.cos(theta_r) -
+    # from page 85 of coimbra thesis
+    theta_prime = 0.5 * np.arccos(np.cos(theta_i) * np.cos(theta_r) -
         np.sin(theta_i) * np.sin(theta_r) * np.cos(phi_r))
 
-    theta_r_prime = 0.5 * np.arccos(np.cos(theta_i) * np.cos(theta_r) -
-        np.sin(theta_i) * np.sin(theta_r))
+    theta_i_prime = theta_prime
+    theta_r_prime = theta_prime
 
     def P(alpha_):
         return np.power(gamma, 2) / \
@@ -153,7 +194,7 @@ def BRIDF_pair(theta_r, phi_r, theta_i, n_0, polarization, parameters):
     G = H(np.pi / 2 - theta_i_prime) * H(np.pi / 2 - theta_r_prime) * \
         G_prime(theta_i) * G_prime(theta_r)
 
-    F_ = F(theta_i, n_0, n, polarization)
+    F_ = F(theta_i_prime, n_0, n, polarization)
 
     return [F_ * G * P_ / (4 * np.cos(theta_i)),
         rho_L / np.pi * W * (1 - A + B) * np.cos(theta_r)]

slider.py
@@ -1,12 +1,17 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from TSTR_fit import BRIDF_plotter
+from TSTR_fit import BRIDF_plotter, BRIDF, BRIDF_radiance_plotter, average_by_angle
 from matplotlib.widgets import Slider, Button
+
+log = False
+show_averaged = True
+angle = 10.
+show_all_angles = False
 
 fig, ax = plt.subplots()
 plt.subplots_adjust(bottom=0.5, top=0.95)
 
-theta_r = np.arange(-90, 90, 0.1)
+theta_r = np.arange(0, 90, 0.1)
 phi_r_0 = 0
 theta_i_0 = 45
 n_0_0 = 1
@@ -14,12 +19,20 @@ polarization_0 = 0.5
 rho_l_0 = 0.5
 n__0 = 1.5
 gamma_0 = 0.05
-
+avg_angle_0 = 4.
 s = BRIDF_plotter(theta_r, phi_r_0, theta_i_0, n_0_0, polarization_0, [rho_l_0, n__0, gamma_0])
-l, = plt.plot(theta_r, s, lw=2, color='red')
-plt.axis([-90., 90., 0, 10])
+s1 = average_by_angle(theta_r, s, avg_angle_0)
 
-t1 = plt.text(15, 9, "peak angle is currently 45.0")
+l, = plt.plot(theta_r, s, lw=2, color='red')
+l1, = plt.plot(theta_r, s1, lw=2, color='blue')
+
+if log:
+    plt.yscale("log")
+    plt.axis([0., 90., 10e-3, 10e2])
+else:
+    plt.axis([0., 90., 0., 10.])
+
+t1 = plt.text(15, 9, "peak angles...")
 
 axcolor = 'lightgoldenrodyellow'
 
@@ -30,6 +43,7 @@ axpolarization = plt.axes([0.25, 0.2, 0.65, 0.03], facecolor=axcolor)
 axrho_l = plt.axes([0.25, 0.15, 0.65, 0.03], facecolor=axcolor)
 axn = plt.axes([0.25, 0.1, 0.65, 0.03], facecolor=axcolor)
 axgamma = plt.axes([0.25, 0.05, 0.65, 0.03], facecolor=axcolor)
+axavg_angle = plt.axes([0.25, 0.00, 0.65, 0.03], facecolor=axcolor)
 
 sphi_r = Slider(axphi_r, "phi_r", -90., 90., valinit=phi_r_0)
 stheta_i = Slider(axtheta_i, "theta_i", 0., 90., valinit=theta_i_0)
@@ -38,6 +52,7 @@ spolarization = Slider(axpolarization, "polarization", 0., 1., valinit=polarizat
 srho_l = Slider(axrho_l, "rho_l", 0., 10., valinit=rho_l_0)
 sn = Slider(axn, "n", 1., 2., valinit=n__0)
 sgamma = Slider(axgamma, "gamma", 0., 1., valinit=gamma_0)
+savg_angle = Slider(axavg_angle,  "average angle", 0, 40, valinit=avg_angle_0)
 
 
 def update(val):
@@ -48,9 +63,12 @@ def update(val):
     rho_l = srho_l.val
     n = sn.val
     gamma = sgamma.val
+    avg_angle = savg_angle.val
     y_data = BRIDF_plotter(theta_r, phi_r, theta_i, n_0, polarization, [rho_l, n, gamma])
+    y1_data = average_by_angle(theta_r, y_data, avg_angle)
     l.set_ydata(y_data)
-    t1.set_text("peak angle is currently " + str(theta_r[y_data.index(max(y_data))]))
+    l1.set_ydata(y1_data)
+    t1.set_text("peak angle is currently " + str(theta_r[y_data.index(max(y_data))]) + "\npeak averaged angle is currently " + str(theta_r[y1_data.index(max(y1_data))]))
     fig.canvas.draw_idle()
 
 sphi_r.on_changed(update)
@@ -60,6 +78,7 @@ spolarization.on_changed(update)
 srho_l.on_changed(update)
 sn.on_changed(update)
 sgamma.on_changed(update)
+savg_angle.on_changed(update)
 
 resetax = plt.axes([0.8, 0.4, 0.1, 0.04])
 button = Button(resetax, 'Reset', color=axcolor, hovercolor='0.975')